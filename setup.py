69cfd5291af376ade419055640d7be30eff060ba
Zadkiel Aslafy-Aharonian <aslafy_z@epitech.eu>
2016-01-10 18:53:39 +0100

    updated python versions tests

--- setup.py
+++ setup.py
@@ -28,8 +28,6 @@ setup(
             'Development Status :: 4 - Beta',
             'Intended Audience :: End Users/Desktop',
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.3",
-            "Programming Language :: Python :: 3.4",
             "Programming Language :: Python :: 3.5",
             'Topic :: Software Development :: Libraries :: Python Modules',
             'Programming Language :: Python',
